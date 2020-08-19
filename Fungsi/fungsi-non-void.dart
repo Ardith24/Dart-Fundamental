dc5c4c10f4bd8a6870ebda89921eedafe3a9d48d
ardith.lutf <ardith.lutfiawan@gmail.com>
2020-08-19 13:16:39 +0700

    back to normal

---
+++ Fungsi/fungsi-non-void.dart
@@ -0,0 +1,17 @@
+double penjumlahan(double a, double b){
+  return a+b;
+}
+
+double pengurangan(double a, double b) => a-b;
+
+double perkalian(double a, double b){
+  return a*b;
+}
+
+double pembagian(double a, double b) => a/b;
+
+void main() {
+  double result = perkalian(3, 2);
+
+  print(result);
+}
--- Fungsi/fungsi.dart
+++ Fungsi/fungsi.dart
@@ -1,4 +1,22 @@
 // Buku: pemrograman Android dengan Flutter
-// Halaman: 89
+// Halaman: 96
 
 //TODO: next ngoding
+
+import 'dart:io';
+
+void main() {
+  int h, m, s;
+  
+  stdout.write('Masukan detik : ');
+  s = int.parse(stdin.readLineSync());
+
+  h = (s / 3600).floor();
+  s = s % 3600;
+  m = (s / 60).floor();
+  s = s % 60;
+  
+  stdout.write(h);
+  stdout.write(m);
+  stdout.write(s);
+}
